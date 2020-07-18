0863c3d78bb23de4e7139dca39b4b4f9b10d248a
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-07-18 02:23:53 -0600

    Updates

---
+++ precache-manifest.2be23585a0a46a2a800fd2825c513b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90603c8f185bed99d317b9ad49fdb75",
+    "url": "https://rvanasa.github.io/elite-navigator/index.html"
+  },
+  {
+    "revision": "9f38eb423e18965f466a",
+    "url": "https://rvanasa.github.io/elite-navigator/static/css/main.43e639a8.chunk.css"
+  },
+  {
+    "revision": "5f8c0e17dcfe654f99de",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/2.886f3a55.chunk.js"
+  },
+  {
+    "revision": "3191b2729bcf54343e77252fda7a3242",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/2.886f3a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f38eb423e18965f466a",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/main.12e0bbf9.chunk.js"
+  },
+  {
+    "revision": "db1a5a69fc752154b027",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/runtime-main.5ed3ceab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76bfd3d88bbed78b3e63eff4d31d7179.js"
+  "https://rvanasa.github.io/elite-navigator/precache-manifest.2be23585a0a46a2a800fd2825c513b5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://rvanasa.github.io/elite-navigator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
