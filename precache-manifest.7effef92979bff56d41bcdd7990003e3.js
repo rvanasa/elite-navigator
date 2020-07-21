c8bf63117a21b619b20b5848275ca2e3058d8527
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-07-21 14:44:05 -0600

    Updates

---
+++ precache-manifest.7effef92979bff56d41bcdd7990003e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5294082bd5b62bd884b309c3812239",
+    "url": "https://rvanasa.github.io/elite-navigator/index.html"
+  },
+  {
+    "revision": "2d1c0d07dada94839cde",
+    "url": "https://rvanasa.github.io/elite-navigator/static/css/main.43e639a8.chunk.css"
+  },
+  {
+    "revision": "bfed8dd7471c5579500e",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/2.1a162557.chunk.js"
+  },
+  {
+    "revision": "3191b2729bcf54343e77252fda7a3242",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/2.1a162557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1c0d07dada94839cde",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/main.15d6f3c8.chunk.js"
+  },
+  {
+    "revision": "db1a5a69fc752154b027",
+    "url": "https://rvanasa.github.io/elite-navigator/static/js/runtime-main.5ed3ceab.js"
+  }
+]);
