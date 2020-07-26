088584ea9e025cf3bbebf60a679a66f255b051e1
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-07-26 14:36:09 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://rvanasa.github.io/elite-navigator/precache-manifest.861b42b8b55eb082743fb445cf908c9f.js"
+  "https://rvanasa.github.io/elite-navigator/precache-manifest.a9f21fcf4b9348389ad6c62535814a63.js"
 );
 
 self.addEventListener('message', (event) => {
